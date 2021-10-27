8949bb28d8ab40bb73af1a5259d7dbc5c99cdaa4
Tim Payne <paynetpayne@gmail.com>
2021-10-27 12:15:16 -0400

    initial email that is sent on every build

--- ide/.vscode/pipeline.groovy
+++ ide/.vscode/pipeline.groovy
@@ -19,6 +19,11 @@ pipeline {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
+                changed {
+                    emailext attachLog: true, body: 'Please go to ${BUILD_URL} and verify the build', 
+                    compressLog: true, subject: 'job \'${JOB_NAME}\' {${BUILD_NUMBER}} is waiting for input', 
+                    to: 'paynetpayne2gmail.com'
+                }
             }
         }
     }
