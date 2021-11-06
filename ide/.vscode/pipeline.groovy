213ef7da2fc33aa3ecf480b6f820a57e15aba164
Tim Payne <paynetpayne@gmail.com>
2021-11-06 15:44:41 -0400

    testing

---
+++ ide/.vscode/pipeline.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Checkout') {
+            steps {
+                // Get some code from a GitHub repository
+                git branch: 'main', url: 'https://github.com/paynet1/jgsu-spring-petclinic.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                sh './mvnw clean package'
+            }
+            post {
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
